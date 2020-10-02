e401ea1fe9e4328668e386bf3b8dfdb4b47d99cd
lihaotian20200217 <1270304241@qq.com>
2020-10-02 10:04:52 +0800

    20201002-10:04

---
+++ 1195.cpp
@@ -0,0 +1,38 @@
+/*
+    LeetCode 1115
+
+*/
+
+class FooBar {
+private:
+    int n;
+    mutex m1, m2;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        m2.lock();
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	m1.lock();
+            printFoo();
+            m2.unlock();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	// printBar() outputs "bar". Do not change or remove this line.
+        	m2.lock();
+            printBar();
+            m1.unlock();
+        }
+    }
+};
