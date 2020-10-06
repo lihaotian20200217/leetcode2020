2c1697b5db314e98780ae0d3bd88f622eb89faf7
lihaotian20200217 <1270304241@qq.com>
2020-10-06 11:13:27 +0800

    20201006-11:13

--- 388.cpp
+++ 388.cpp
@@ -7,71 +7,34 @@ using namespace std;
 
 class Solution {
 public:
+    int res = 0;
     int lengthLongestPath(string input) {
-        // 用栈
-        stack<char> stk;
-        int res = -1, count = 0, f = 0, layer = 0;
-        for (int i = 0; i < input.size(); i++)
+        input += '\n';
+        stack<pair<int,int>> sk;
+        int i = 0, tab = 0, cur = 0;
+        while (i < input.size())
         {
-            if (input[i] == '.') f++;
-            if (input[i] != '\n') stk.push(input[i]);
-            else
+            int end = input.find('\n',i);
+            string tmp = input.substr(i,end-i);
+            i = end + 1;
+            pair<int,int> eme = make_pair(tmp.size(),tab);
+            tab = 0;
+            while (i < input.size() && input[i] == '\t')
             {
-                if (input[i] == '.') f++;
-                count = 0;
-                // 计算是第几层 count 
-                int j = i, sum = 0;
-                while (j < input.size()-1)
-                {
-                    if (input[j] == '\n')
-                    {
-                        j++;
-                    }
-                    else if (input[j] == '\t')
-                    {
-                        j++;
-                        count++;
-                        //if (count == layer+1) break;
-                    }
-                    else if (input[j] == ' ')
-                    {
-                        sum++;
-                        j++;
-                        if (sum == 4)
-                        {
-                            sum = 0;
-                            count++;
-                            if (count == layer+1) break;
-                        }
-                    }
-                    else break;
-                }
-                i = j - 1;
-
-                // 如果 count > layer, 更新layer push('\\'),继续
-                if (count > layer)
-                {
-                    layer = count;
-                    stk.push('\\');
-                }
-                else
-                {
-                    // 反之，出栈直到 count = layer-1
-                    res = max(res,(int)stk.size());
-                    //cout << res << endl;
-                    while (layer >= count)
-                    {
-                        layer--;
-                        while (stk.top() != '\\') stk.pop();
-                        stk.pop(); 
-                    } //cout << stk.top() << endl;
-                    stk.push('\\'); // 最后一次的不能弹出
-                    layer = count; // 恢复到count层
-                }
+                i++, tab++;
             }
-        } //cout << input.size();
-        res = max(res,(int)stk.size());
-        if (!f) return 0;
+            while (!sk.empty() && sk.top().second >= eme.second)
+            {
+                cur -= sk.top().first;
+                sk.pop();
+            }
+            if (sk.empty() || sk.top().second < eme.second)
+            {
+                cur += tmp.size();
+                sk.push(eme);
+            }
+            if (tmp.find('.') != -1) res = max(res,cur+(int)sk.size()-1);
+        }
         return res;
     }
 };
