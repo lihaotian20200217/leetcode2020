c10408d4e79c795f4ef25c5bb091f88be98f9573
lihaotian20200217 <1270304241@qq.com>
2020-10-09 09:23:36 +0800

    20201009-09:23

--- 1604.cpp
+++ 1604.cpp
@@ -7,37 +7,49 @@ using namespace std;
 
 class Solution {
 public:
-    static bool cmp(vector<int>& p1, vector<int>& p2)
+    //static bool cmp(vector<int>& p1, vector<int>& p2)
+    static bool cmp(int& p1, int& p2)
     {
+        /*
         if (p1[0] < p2[0]) return true;
         else if (p1[0] == p2[0]) return p1[1] <= p2[1];
         return false;
+        */
+        return p1 <= p2;
     }
     vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
-        unordered_map<string,vector<vector<int>>> m;
-        set<string> Set;
+        //map<string,vector<vector<int>>> m;
+        map<string,vector<int>> m;
+        //set<string> Set;
         vector<string> res;
         for (int i = 0; i < keyName.size(); i++)
         {
-            int a = (int)(keyTime[i][0] - '0') * 10 + (int)(keyTime[i][1] - '0');
-            int b = (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
-            m[keyName[i]].push_back({a,b});
+            //int a = (int)(keyTime[i][0] - '0') * 10 + (int)(keyTime[i][1] - '0');
+            //int b = (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
+            //m[keyName[i]].push_back({a,b});
+            int a = (int)(keyTime[i][0] - '0') * 1000 + (int)(keyTime[i][1] - '0') * 100 + (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
+            m[keyName[i]].push_back(a);
         }
         for (auto item : m)
         {
             sort(item.second.begin(),item.second.end(),cmp);
             for (int i = 2; i < item.second.size(); i++)
             {
-                if (item.second[i][0] >= item.second[i-2][0])
+                if (item.second[i]/100 >= item.second[i-2]/100)
                 {
-                    if (!(item.second[i][0]-item.second[i-2][0] > 1 || (item.second[i][0]-item.second[i-2][0] == 1 && item.second[i][1] > item.second[i-2][1]))) Set.insert(item.first);
+                    if (!(item.second[i]/100-item.second[i-2]/100 > 1 || (item.second[i]/100-item.second[i-2]/100 == 1 && item.second[i]%100 > item.second[i-2]%100)))
+                    {
+                        res.push_back(item.first);
+                        break;
+                    } 
                 }
             }
         }
-        for (auto item : Set) res.push_back(item);
+        //for (auto item : Set) res.push_back(item);
         return res;
     }
 };
+
 int main()
 {
     vector<string> keyName = {"daniel","daniel","daniel","luis","luis","luis","luis"};
