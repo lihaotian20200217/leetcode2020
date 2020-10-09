3df28a5e84b73b113f06654abceec96a0eb62f4a
lihaotian20200217 <1270304241@qq.com>
2020-10-09 09:27:35 +0800

    20201009-09:27

--- 1604.cpp
+++ 1604.cpp
@@ -7,32 +7,17 @@ using namespace std;
 
 class Solution {
 public:
-    //static bool cmp(vector<int>& p1, vector<int>& p2)
-    static bool cmp(int& p1, int& p2)
-    {
-        /*
-        if (p1[0] < p2[0]) return true;
-        else if (p1[0] == p2[0]) return p1[1] <= p2[1];
-        return false;
-        */
-        return p1 <= p2;
-    }
     vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
-        //map<string,vector<vector<int>>> m;
         map<string,vector<int>> m;
-        //set<string> Set;
         vector<string> res;
         for (int i = 0; i < keyName.size(); i++)
         {
-            //int a = (int)(keyTime[i][0] - '0') * 10 + (int)(keyTime[i][1] - '0');
-            //int b = (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
-            //m[keyName[i]].push_back({a,b});
             int a = (int)(keyTime[i][0] - '0') * 1000 + (int)(keyTime[i][1] - '0') * 100 + (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
             m[keyName[i]].push_back(a);
         }
         for (auto item : m)
         {
-            sort(item.second.begin(),item.second.end(),cmp);
+            sort(item.second.begin(),item.second.end());
             for (int i = 2; i < item.second.size(); i++)
             {
                 if (item.second[i]/100 >= item.second[i-2]/100)
@@ -45,7 +30,6 @@ public:
                 }
             }
         }
-        //for (auto item : Set) res.push_back(item);
         return res;
     }
 };
@@ -56,5 +40,9 @@ int main()
     vector<string> keyTime = {"10:00","10:40","11:00","09:00","11:00","13:00","15:00"};
     Solution A;
     keyName = A.alertNames(keyName,keyTime);
+    for (int i = 0; i < keyName.size(); i++)
+    {
+        cout << keyName[i] << " ";
+    }
     return 0;
 }
