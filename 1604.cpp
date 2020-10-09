2726dd101ff2dca56a79801213c3d024ba76ecf5
lihaotian20200217 <1270304241@qq.com>
2020-10-09 09:10:45 +0800

    20201009-09:10

--- 1604.cpp
+++ 1604.cpp
@@ -7,43 +7,37 @@ using namespace std;
 
 class Solution {
 public:
+    static bool cmp(vector<int>& p1, vector<int>& p2)
+    {
+        if (p1[0] < p2[0]) return true;
+        else if (p1[0] == p2[0]) return p1[1] <= p2[1];
+        return false;
+    }
     vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
-        unordered_map<string,queue<string>> m;
+        unordered_map<string,vector<vector<int>>> m;
         set<string> Set;
         vector<string> res;
-        //string pre0, pre1, aft0, aft1;
         for (int i = 0; i < keyName.size(); i++)
         {
-            if (m.find(keyName[i]) != m.end() && m[keyName[i]].size() == 2)
+            int a = (int)(keyTime[i][0] - '0') * 10 + (int)(keyTime[i][1] - '0');
+            int b = (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
+            m[keyName[i]].push_back({a,b});
+        }
+        for (auto item : m)
+        {
+            sort(item.second.begin(),item.second.end(),cmp);
+            for (int i = 2; i < item.second.size(); i++)
             {
-                string ss = m[keyName[i]].front();
-                //cout << ss[0] << " " << ss[1] << endl;
-                //pre0 = "", pre1 = "", aft0 = "", aft1 = "";
-                //pre0 += ss[0] + ss[1];
-                //aft0 += keyTime[i][0] + keyTime[i][1];
-                //string pre1 = "" + m[keyName[i]].front()[3] + m[keyName[i]].front()[4];
-                //pre1 += ss[3] + ss[4];
-                //aft1 += keyName[i][3] + keyName[i][4];
-                //int a = atoi(pre0.c_str()), b = atoi(aft0.c_str());
-                //int c = atoi(pre1.c_str()), d = atoi(aft1.c_str());
-                int a = (int)(ss[0] - '0') * 10 + (int)(ss[1] - '0');
-                int b = (int)(keyTime[i][0] - '0') * 10 + (int)(keyTime[i][1] - '0');
-                int c = (int)(ss[3] - '0') * 10 + (int)(ss[4] - '0');
-                int d = (int)(keyTime[i][3] - '0') * 10 + (int)(keyTime[i][4] - '0');
-                if (b >= a)
+                if (item.second[i][0] >= item.second[i-2][0])
                 {
-                    if (!(b-a > 1 || (b-a == 1 && d > c))) Set.insert(keyName[i]);
+                    if (!(item.second[i][0]-item.second[i-2][0] > 1 || (item.second[i][0]-item.second[i-2][0] == 1 && item.second[i][1] > item.second[i-2][1]))) Set.insert(item.first);
                 }
-                
-                m[keyName[i]].pop();
             }
-            m[keyName[i]].push(keyTime[i]);
         }
         for (auto item : Set) res.push_back(item);
         return res;
     }
 };
-
 int main()
 {
     vector<string> keyName = {"daniel","daniel","daniel","luis","luis","luis","luis"};
