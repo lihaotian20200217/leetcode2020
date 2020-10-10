5956b0bfc25abe61ee25fcf3e9245584c6e2b9bd
lihaotian20200217 <1270304241@qq.com>
2020-10-10 09:31:03 +0800

    20201010-09:30

--- 1234.cpp
+++ 1234.cpp
@@ -12,32 +12,24 @@ public: // 'Q' 'w' 'E' 'R'
 		vector<vector<int>> arr(len+1,vector<int>(4,0));
 		for (int i = 1; i <= len; i++)
 		{
+			arr[i][0] = arr[i-1][0];
+			arr[i][1] = arr[i-1][1];
+			arr[i][2] = arr[i-1][2];
+			arr[i][3] = arr[i-1][3];
 			if (s[i-1] == 'Q')
 			{
 				arr[i][0] = arr[i-1][0] + 1;
-				arr[i][1] = arr[i-1][1];
-				arr[i][2] = arr[i-1][2];
-				arr[i][3] = arr[i-1][3];
 			}
 			else if (s[i-1] == 'W')
 			{
-				arr[i][0] = arr[i-1][0];
 				arr[i][1] = arr[i-1][1] + 1;
-				arr[i][2] = arr[i-1][2];
-				arr[i][3] = arr[i-1][3];
 			}
 			else if (s[i-1] == 'E')
 			{
-				arr[i][0] = arr[i-1][0];
-				arr[i][1] = arr[i-1][1];
 				arr[i][2] = arr[i-1][2] + 1;
-				arr[i][3] = arr[i-1][3];
 			}
 			else
 			{
-				arr[i][0] = arr[i-1][0];
-				arr[i][1] = arr[i-1][1];
-				arr[i][2] = arr[i-1][2];
 				arr[i][3] = arr[i-1][3] + 1;
 			}
 		}
