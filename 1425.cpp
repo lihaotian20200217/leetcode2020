355d3f34db4cef2c1d572d77dbf9dd6f991381a7
lihaotian20200217 <1270304241@qq.com>
2020-11-20 08:51:18 +0800

    20201120-08:51

---
+++ 1425.cpp
@@ -0,0 +1,50 @@
+/*
+	给你一个整数数组 nums 和一个整数 k ，请你返回 非空 子序列元素和的最大值，子序列需要满足：子序列中每两个 相邻 的整数 nums[i] 和 nums[j] ，它们在原数组中的下标 i 和 j 满足 i < j 且 j - i <= k 。
+
+数组的子序列定义为：将数组中的若干个数字删除（可以删除 0 个数字），剩下的数字按照原本的顺序排布。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/constrained-subsequence-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+输入：nums = [10,2,-10,5,20], k = 2
+输出：37
+解释：子序列为 [10, 2, 5, 20] 。
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+	public:
+		/*
+			dp[i]表示截止到i子序列的最大和
+			dp[i] = max(dp[i-j],nums[i]); j = 1,2...k
+			直接遍历浪费时间所以用单调栈维护最大值的索引
+		*/
+		int constrainedSubsetSum(vector<int>& nums, int k)
+		{
+			int len = nums.size();
+			vector<int> dp(len,0);
+			dp[0] = nums[0];
+			for (int i = 1; i < len; i++)
+			{
+				dp[i] = nums[i];
+				for (int j = 1; j <= min(k,i); j++)
+				{
+					dp[i] = max(dp[i],dp[i-j]+nums[i]);
+				}
+			}
+			sort(dp.begin(),dp.end());
+			return dp[len-1];
+		}
+};
+
+int main()
+{
+	Solution A;
+	vector<int> arr = {10,2,-10,5,20};
+	cout << A.constrainedSubsetSum(arr,2) << endl;
+	return 0;
+}
